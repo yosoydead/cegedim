20eb5567d971335095326e22e48d8ed639029569
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2022-05-09 14:35:35 +0300

    initial display of data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,53 @@
 <template>
   <div class="hello">
     <button @click="testClick">Test click</button>
+    <div v-if="showCharacters">
+      <p>maxime {{ max }}</p>
+      <letter-box
+        v-for="letter in modulos"
+        :key="letter"
+        :charToDisplay="letter"
+      ></letter-box>
+    </div>
   </div>
 </template>
 
 <script>
-// import { createAsciiAlphabet } from '../Utils/createAsciiAlphabet'
+import { createAsciiAlphabet } from '../Utils/createAsciiAlphabet'
 import { calculateHundredsClass } from '../Utils/calculateNumbersClass'
+import LetterBox from './LetterBox/LetterBox.vue'
 
 export default {
   name: 'HelloWorld',
+  components: {
+    'letter-box': LetterBox
+  },
+  created () {
+    this.asciiRange = createAsciiAlphabet()
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      numsArray: [110, 120, 130, 280, 780, 510, 505, 605, 720, 210, 275]
+      numsArray: [110, 120, 130, 280, 780, 510, 505, 605, 720, 210, 275],
+      max: [],
+      modulos: [],
+      showCharacters: false,
+      asciiRange: []
     }
   },
   methods: {
     testClick: function (event) {
       const result = calculateHundredsClass(this.numsArray)
-      this.getMaxValues(result)
+      this.max = this.getMaxValues(result)
+      this.showCharacters = true
     },
     getMaxValues: function (numberClasses) {
       const maxes = []
 
       Object.keys(numberClasses).forEach(key => {
-        console.log(numberClasses[key])
-        maxes.push(Math.max(...numberClasses[key]))
+        const max = Math.max(...numberClasses[key])
+        maxes.push(Math.max(max))
+        this.modulos.push(String.fromCharCode(this.asciiRange[max % 26]))
       })
 
       return maxes
---
+++ src/components/LetterBox/LetterBox.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>value to display: {{ charToDisplay }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LetterBox',
+  props: {
+    charToDisplay: {
+      type: String,
+      default: 'no value provided'
+    }
+  }
+}
+</script>
